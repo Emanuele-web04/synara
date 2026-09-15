Axl47/synapse

fix: address Codex provider-instance review findings (round 9)
- stopSession routes deleted provider instances via the persisted binding
  (new allowDeleted option on resolveRoutableSession) so teardown reaches
  the adapter and removes the binding after an instance is removed from
  settings
- localStorage plaintext instance secrets survive until the initial
  server-settings migration has shipped them to the server; redaction now
  runs after migration completes instead of on mount
- redacted instance server passwords show a saved placeholder, ignore
  empty debounce commits, and gain a Clear button that removes the stored
  secret

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01Q2AdgNLoV5CY12V4HibpBi

apps/server/src/provider/Layers/ProviderService.ts
@@ -1202,12 +1202,37 @@ const makeProviderService = (options?: ProviderServiceLiveOptions) =>
         { discard: true, concurrency: "unbounded" },
       ).pipe(Effect.asVoid);
 
+    const providerInstanceExists = (input: {
+      readonly operation: string;
+      readonly instanceId: string;
+      readonly provider: string;
+    }) =>
+      serverSettings.getSettings.pipe(
+        Effect.mapError((cause) =>
+          toValidationError(input.operation, "Failed to load provider instance settings.", cause),
+        ),
+        Effect.map((settings) =>
+          Boolean(
+            resolveProviderInstance(settings, {
+              instanceId: input.instanceId,
+              ...providerKindConstraint(input.provider),
+            }),
+          ),
+        ),
+      );
+
     const resolveRoutableSession = (input: {
       readonly threadId: ThreadId;
       readonly operation: string;
       readonly allowRecovery: boolean;
       /** Stop/cleanup paths must still route sessions of disabled instances. */
       readonly allowDisabled?: boolean;
+      /**
+       * Stop/cleanup paths must also tear down runtimes whose provider
+       * instance was deleted from settings; those route by the persisted
+       * binding (or live session) instead of failing instance resolution.
+       */
+      readonly allowDeleted?: boolean;
     }) =>
       Effect.gen(function* () {
         const bindingOption = yield* directory.getBinding(input.threadId);
@@ -1217,10 +1242,26 @@ const makeProviderService = (options?: ProviderServiceLiveOptions) =>
           // the provider binding, but the adapter already owns a live session.
           const live = yield* findLiveSession(input.threadId);
           if (live) {
+            const liveProviderInstanceId = live.session.providerInstanceId ?? live.session.provider;
+            if (input.allowDeleted === true) {
+              const exists = yield* providerInstanceExists({
+                operation: input.operation,
+                instanceId: liveProviderInstanceId,
+                provider: live.session.provider,
+              });
+              if (!exists) {
+                return {
+                  adapter: live.adapter,
+                  threadId: input.threadId,
+                  providerInstanceId: liveProviderInstanceId,
+                  isActive: true,
+                } as const;
+              }
+            }
             const resolved = yield* resolveLaunchProviderInstance({
               operation: input.operation,
               provider: live.session.provider,
-              providerInstanceId: live.session.providerInstanceId ?? live.session.provider,
+              providerInstanceId: liveProviderInstanceId,
               ...(input.allowDisabled !== undefined ? { allowDisabled: input.allowDisabled } : {}),
             });
             return {
@@ -1236,6 +1277,23 @@ const makeProviderService = (options?: ProviderServiceLiveOptions) =>
           );
         }
         const bindingProviderInstanceId = providerInstanceIdFromBinding(binding);
+        if (input.allowDeleted === true) {
+          const exists = yield* providerInstanceExists({
+            operation: input.operation,
+            instanceId: bindingProviderInstanceId,
+            provider: binding.provider,
+          });
+          if (!exists) {
+            const adapter = yield* getAdapterForBinding(binding);
+            const isActive = yield* adapter.hasSession(input.threadId);
+            return {
+              adapter,
+              threadId: input.threadId,
+              providerInstanceId: bindingProviderInstanceId,
+              isActive,
+            } as const;
+          }
+        }
         const resolved = yield* resolveLaunchProviderInstance({
           operation: input.operation,
           ...providerKindConstraint(binding.provider),
@@ -1834,6 +1892,7 @@ const makeProviderService = (options?: ProviderServiceLiveOptions) =>
           operation: "ProviderService.stopSession",
           allowRecovery: false,
           allowDisabled: true,
+          allowDeleted: true,
         });
         if (routed.isActive) {
           yield* routed.adapter.stopSession(routed.threadId);

Emanuele-web04/synara

apps/server/src/provider/Layers/ProviderService.ts
@@ -2250,12 +2250,37 @@ const makeProviderService = (options?: ProviderServiceLiveOptions) =>
         { discard: true, concurrency: "unbounded" },
       ).pipe(Effect.asVoid);
 
+    const providerInstanceExists = (input: {
+      readonly operation: string;
+      readonly instanceId: string;
+      readonly provider: string;
+    }) =>
+      serverSettings.getSettings.pipe(
+        Effect.mapError((cause) =>
+          toValidationError(input.operation, "Failed to load provider instance settings.", cause),
+        ),
+        Effect.map((settings) =>
+          Boolean(
+            resolveProviderInstance(settings, {
+              instanceId: input.instanceId,
+              ...providerKindConstraint(input.provider),
+            }),
+          ),
+        ),
+      );
+
     const resolveRoutableSession = (input: {
       readonly threadId: ThreadId;
       readonly operation: string;
       readonly allowRecovery: boolean;
       /** Stop/cleanup paths must still route sessions of disabled instances. */
       readonly allowDisabled?: boolean;
+      /**
+       * Stop/cleanup paths must also tear down runtimes whose provider
+       * instance was deleted from settings; those route by the persisted
+       * binding (or live session) instead of failing instance resolution.
+       */
+      readonly allowDeleted?: boolean;
     }) =>
       Effect.gen(function* () {
         const binding = Option.getOrUndefined(yield* directory.getBinding(input.threadId));
@@ -2264,11 +2289,27 @@ const makeProviderService = (options?: ProviderServiceLiveOptions) =>
           // the provider binding, but the adapter already owns a live session.
           const live = yield* findLiveSession(input.threadId);
           if (live) {
-            const providerInstanceId = live.session.providerInstanceId ?? live.session.provider;
+            const liveProviderInstanceId = live.session.providerInstanceId ?? live.session.provider;
+            if (input.allowDeleted === true) {
+              const exists = yield* providerInstanceExists({
+                operation: input.operation,
+                instanceId: liveProviderInstanceId,
+                provider: live.session.provider,
+              });
+              if (!exists) {
+                return {
+                  adapter: live.adapter,
+                  threadId: input.threadId,
+                  providerInstanceId: liveProviderInstanceId,
+                  isActive: true,
+                  lifecycleGeneration: lifecycle.currentGeneration(input.threadId),
+                } as const;
+              }
+            }
             const resolved = yield* resolveLaunchProviderInstance({
               operation: input.operation,
               provider: live.session.provider,
-              providerInstanceId,
+              providerInstanceId: liveProviderInstanceId,
               ...(input.allowDisabled !== undefined ? { allowDisabled: input.allowDisabled } : {}),
             });
             return {
@@ -2291,6 +2332,28 @@ const makeProviderService = (options?: ProviderServiceLiveOptions) =>
           });
         }
         const persistedProviderInstanceId = providerInstanceIdFromBinding(binding);
+        if (input.allowDeleted === true) {
+          const exists = yield* providerInstanceExists({
+            operation: input.operation,
+            instanceId: persistedProviderInstanceId,
+            provider: binding.provider,
+          });
+          if (!exists) {
+            const adapter = yield* getAdapterForBinding(
+              binding,
+              input.allowDisabled !== undefined
+                ? { allowDisabled: input.allowDisabled }
+                : undefined,
+            );
+            return {
+              adapter,
+              threadId: input.threadId,
+              providerInstanceId: persistedProviderInstanceId,
+              isActive: yield* adapter.hasSession(input.threadId),
+              lifecycleGeneration: binding.lifecycleGeneration,
+            } as const;
+          }
+        }
         const resolved = input.allowRecovery
           ? yield* resolveLaunchProviderInstance({
               operation: input.operation,
@@ -3478,6 +3541,7 @@ const makeProviderService = (options?: ProviderServiceLiveOptions) =>
               operation: "ProviderService.stopSession",
               allowRecovery: false,
               allowDisabled: true,
+              allowDeleted: true,
             }).pipe(
               Effect.catchTag("ProviderValidationError", (error) =>
                 error.issue.includes("no persisted provider binding exists")